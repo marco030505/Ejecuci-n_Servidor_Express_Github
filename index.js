f6021aa62a766e335b828c8bd26ac486a5877b12
Marco Antonio Aguirre <vidacamarco@gmail.com>
2026-03-24 22:21:54 -0700

    Primer endpoint GET en una REST API

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+app.get("/usuario", (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: "Marco",
+    rol: "Administrador",
+  };
+
+  res.json(usuario);
 });
 
 app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+  console.log("Servidor corriendo en http://localhost:3000");
 });
